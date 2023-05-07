0cb79795be4b5fce1bdbaf53ad1225979e251a89
Wermison Sousa <wermison_15@hotmail.com>
2023-05-07 12:35:09 -0300

    Adicionado contador de linhas

--- assets/js/contador-de-palavras-e-caracteres.js
+++ assets/js/contador-de-palavras-e-caracteres.js
@@ -1,6 +1,7 @@
 let input = document.querySelector('#input-area');
 let areaCharacters = document.querySelector('#result-characters');
 let areaWords = document.querySelector('#result-words');
+let areaLines = document.querySelector('#result-lines');
 let btnClear = document.querySelector('#btn-clear');
 
 input.addEventListener('input', () => {
@@ -11,6 +12,10 @@ input.addEventListener('input', () => {
     const numberWords = input.value.trim() === "" ? 0 : words.length;
     areaWords.textContent = numberWords;
 
+    let lines = input.value.trim().split("\n");
+    const numberLines = input.value.trim() === "" ? 0 : lines.length;
+    areaLines.textContent = numberLines;
+
     btnClear.style.display = 'block'
 });
 
@@ -18,5 +23,6 @@ btnClear.addEventListener('click', () => {
     input.value = "";
     areaWords.textContent = "0";
     areaCharacters.textContent = "0";
+    areaLines.textContent = "0"
     btnClear.style.display = 'none'
 })
