362d42da6cdb633bc4b1b3afff6a99dec68a1fd2
Wermison Sousa <wermison_15@hotmail.com>
2023-05-20 12:35:35 -0300

    home and search

---
+++ assets/js/data.js
@@ -0,0 +1,78 @@
+let data = [
+    { 
+        title: "Calculadora de Horas", 
+        desc: "Ferramenta online grátis para calcular horas - use nosso app para calcular horas trabalhadas, hora extras e muito mais.", 
+        link: "/pages/gerador-de-utm.html" 
+    },
+    { 
+        title: "Calculadora de porcentagem", 
+        desc: "Calculadora de porcentagem online grátis. Ferramenta online que permite calcular a porcentagem de valores de várias formas gratuitamente.", 
+        link: "/pages/calculadora-de-porcentagem.html" 
+    },
+    { 
+        title: "Calcular regra de 3 Simples", 
+        desc: "Calculadora de regra de 3 simples online grátis. Ferramenta online que permite calcular a regra de três simples gratuitamente.", 
+        link: "/pages/calculadora-de-regra-de-3-simples.html" 
+    },
+    { 
+        title: "Calcular o tamanho de terrenos com 3 lados diferentes", 
+        desc: "Calcule abaixo o tamanho de terrenos com formato irregular (3 lados diferentes). Aprenda como o cálculo é feito - Aprenda a calcular área de imóveis", 
+        link: "/pages/calcular-area-de-terrenos-irregulares-3-lados-diferentes.html" 
+    },
+    { 
+        title: "Calcular o tamanho de terrenos com 4 lados diferentes", 
+        desc: "Calcule abaixo o tamanho de terrenos com formato irregular (4 lados diferentes). Aprenda como o cálculo é feito - Aprenda a calcular área de imóveis", 
+        link: "/pages/calcular-area-de-terrenos-irregulares-4-lados-diferentes.html" 
+    },
+    { 
+        title: "Calcular metros quadrados", 
+        desc: "Ferramenta simple e grátis para calcular metros quadrado - calculadora de metros quadrados - m2", 
+        link: "/pages/calcular-area-de-terrenos-irregulares-4-lados-diferentes.html" 
+    },
+    { 
+        title: "Contador de palavras e caracteres", 
+        desc: "Conte a quantidade de caracteres e de palavras que seu texto contém - Ferramenta online para contar letras e palavras", 
+        link: "/pages/contador-de-palavras-e-caracteres.html" 
+    },
+    { 
+        title: "Transformar em Maúsculas e Minúsculas", 
+        desc: "Ferramenta online grátis para converter ou transformar letras maiúsculas e minúsculas de forma rápida e simples", 
+        link: "/pages/converter-palavras-em-maiusculas-e-minusculas.html" 
+    },
+    { 
+        title: "Cronômento online", 
+        desc: "Cronômetro Online para marcar intervalos de tempo.", 
+        link: "/pages/cronometro-online.html" 
+    },
+    { 
+        title: "Gerador de CPF Válido", 
+        desc: "Gerador de CPF online e gratuito - Gere CPF válidos para usar em suas aplicações - Valide CPF com essa ferramenta simples e gratuita", 
+        link: "/pages/gerar-cpf-valido.html" 
+    },
+    { 
+        title: "Gerador de recibos", 
+        desc: "Gerador de recibos online e gratuito - Ferramenta simples e prática para gerar seu recibo online", 
+        link: "/pages/gerador-de-recibos-online.html" 
+    },
+    { 
+        title: "Gerador de senhas seguras", 
+        desc: "Gere senhas fortes e seguras rápido e fácil com essa ferramentas online grátis - Gerador de senhas online", 
+        link: "/pages/gerador-de-senhas-seguras.html" 
+    },
+    { 
+        title: "Gerador de UTM", 
+        desc: "Este gerador de UTM para campanhas permite você gerar facilmente, com parâmetros e medidas usadas pelo Google Analytics e Outros", 
+        link: "/pages/gerador-de-utm.html" 
+    },
+    { 
+        title: "Inverter Texto", 
+        desc: "Ferramenta online grátis para inverter texto, colocar texto de trás para frente online - ex: abc -> cba", 
+        link: "/pages/inverter-texto.html" 
+    },
+    { 
+        title: "Sortear times", 
+        desc: "Ferramenta para sortear times para partidas de futebol ou qualquer outro esporte", 
+        link: "/pages/sortear-times.html" 
+    }
+];
+
---
+++ assets/js/search-home.js
@@ -0,0 +1,49 @@
+let search = document.querySelector('#search');
+let btnSearch = document.querySelector('#pesquisar')
+
+data.map((item, index) => {
+    let postItem = document.querySelector('.models .post-item').cloneNode(true);
+
+    postItem.querySelector('#title').innerHTML = item.title;
+    postItem.querySelector('#desc').innerHTML = item.desc;
+    postItem.querySelector('a').href = item.link;
+
+    document.querySelector('.post-area').append(postItem);
+});
+
+// btnSearch.addEventListener('click', listSearch);
+
+
+function listSearch() {
+    document.querySelector('.post-area').innerHTML = "";
+
+    if (search.value == '') {
+        data.map((item, index) => {
+            let postItem = document.querySelector('.models .post-item').cloneNode(true);
+        
+            postItem.querySelector('#title').innerHTML = item.title;
+            postItem.querySelector('#desc').innerHTML = item.desc;
+            postItem.querySelector('a').href = item.link;
+        
+            document.querySelector('.post-area').append(postItem);
+        });
+    } else {
+        data.filter((item) => {
+            if ((item.title.toLocaleLowerCase()).includes((search.value.toLocaleLowerCase())) || (item.desc.toLocaleLowerCase()).includes((search.value.toLocaleLowerCase()))) {
+                let postItem = document.querySelector('.models .post-item').cloneNode(true);
+    
+                postItem.querySelector('#title').innerHTML = item.title;
+                postItem.querySelector('#desc').innerHTML = item.desc;
+                postItem.querySelector('a').href = item.link;
+    
+                document.querySelector('.post-area').append(postItem);
+            }
+    
+        });
+    }
+
+
+
+};
+
+search.addEventListener('input', listSearch);
