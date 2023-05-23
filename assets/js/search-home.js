f95c2c453544c46570b9321d588d90ec149d9b5f
Wermison Sousa <wermison_15@hotmail.com>
2023-05-23 17:28:27 -0300

    home e search new

--- assets/js/search-home.js
+++ assets/js/search-home.js
@@ -1,44 +1,30 @@
 let search = document.querySelector('#search');
-let btnSearch = document.querySelector('#pesquisar')
-
-dataPosts.map((item, index) => {
-    let postItem = document.querySelector('.models .post-item').cloneNode(true);
-
-    postItem.querySelector('#title').innerHTML = item.title;
-    postItem.querySelector('#desc').innerHTML = item.desc;
-    postItem.querySelector('a').href = item.link;
-
-    document.querySelector('.post-area').append(postItem);
-});
+let btnSearch = document.querySelector('#pesquisar');
 
 btnSearch.addEventListener('click', listSearch);
 
+let postList = document.querySelector('.post-area').cloneNode(true);
 
 function listSearch() {
+    
     document.querySelector('.post-area').innerHTML = "";
 
     if (search.value == '') {
-        dataPosts.map((item, index) => {
-            let postItem = document.querySelector('.models .post-item').cloneNode(true);
-        
-            postItem.querySelector('#title').innerHTML = item.title;
-            postItem.querySelector('#desc').innerHTML = item.desc;
-            postItem.querySelector('a').href = item.link;
-        
-            document.querySelector('.post-area').append(postItem);
-        });
+
+        document.querySelector('.post-area').append(postList);
+
     } else {
         dataPosts.filter((item) => {
             if ((item.title.toLocaleLowerCase()).includes((search.value.toLocaleLowerCase())) || (item.desc.toLocaleLowerCase()).includes((search.value.toLocaleLowerCase()))) {
                 let postItem = document.querySelector('.models .post-item').cloneNode(true);
-    
+
                 postItem.querySelector('#title').innerHTML = item.title;
                 postItem.querySelector('#desc').innerHTML = item.desc;
                 postItem.querySelector('a').href = item.link;
-    
+
                 document.querySelector('.post-area').append(postItem);
             }
-    
+
         });
     }
 
